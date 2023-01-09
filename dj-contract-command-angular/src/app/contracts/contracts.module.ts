4dde4f754cab5396babace20120145f634752887
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2023-01-09 13:11:53 -0500

    add contract module and contract form

---
+++ dj-contract-command-angular/src/app/contracts/contracts.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ContractFormComponent } from './contract-form/contract-form.component';
+
+
+@NgModule({
+  declarations: [
+    ContractFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContractsModule { }
