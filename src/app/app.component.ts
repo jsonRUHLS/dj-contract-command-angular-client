9d3f20f51bde95dc4bc7251170639b34367dddf9
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2023-01-09 14:01:23 -0500

    update name of project and gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'dj-contract-command';
+  title = 'dj-contract-command-angular-client';
 }
